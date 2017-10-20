14912887c58d295fdb226bff8a3b211ed5e46a95
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-10-19 22:02:07 -0600

    holy shit heroku blows....

--- react-backend/routes/quilts.js
+++ react-backend/routes/quilts.js
@@ -4,10 +4,8 @@ const quiltRouter = express.Router()
 const Quilt = require('../db/models/quilts');
 
 quiltRouter.get('/', (req, res) => {
-  console.log(' We should hit the quilts routere', );
   Quilt.find()
   .then((results) => {
-    console.log(' DO WE HAVE RESULTS???', results);
     res.json({results});
   })
   .catch((err) => {
