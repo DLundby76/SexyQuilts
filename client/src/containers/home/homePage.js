ea2d7682f1ba647d55431c2168e292637387c117
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2018-01-16 13:50:19 -0700

    working

--- client/src/containers/home/homePage.js
+++ client/src/containers/home/homePage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import Carousel from '../../components/carouselComponent/index.js';
 import Footer from '../../components/footer/index.js';
 import logo from '../../logo_final.png';
 
@@ -28,14 +27,14 @@ class Home extends Component {
       <div className='page'>
         <div  className="jumbotron" style={homeStyles}>
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Not your grandmother's quilts.</h2>
+          <h2>These are not your grandmother's quilts.</h2>
         </div>
         {this.state.users.map(user =>
         <div key={user.id}>
           {user.username}
 
         </div>)}
-        {/* <Carousel /> */}
+
         <Footer />
       </div>
     );
